0b5bef639e74872cf2a316aec8c18f6304df7a92
Alexey <ax.chinaev@yandex.ru>
2023-09-20 10:59:45 +0300

    done

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 const content = document.createElement('span')
 content.className = "modal"
-content.innerText = "I'm blue\n" +
-    "Da ba dee da ba di\n" +
-    "Da ba dee da ba di\n" +
-    "Da ba dee da ba di\n" +
-    "Da ba dee da ba di\n" +
-    "Da ba dee da ba di\n" +
-    "Da ba dee da ba di\n" +
-    "Da ba dee da ba di\n"
+content.innerText = "A lot of interesting text"
 
 const root = document.getElementById("root")
 root.appendChild(content)
+const modal = document.getElementsByClassName("modal")[0]
 
-root.addEventListener("click", (e) => {
-    const modal = document.getElementsByClassName("modal")[0]
-    if (e.target !== modal) {
-        document.getElementById("root").removeChild(modal)
-    }
+root.addEventListener("click", e => {
+    root.removeChild(modal)
+})
+
+modal.addEventListener("click", e => {
+    e.stopPropagation()
 })
